14fe54d21667dc6a84888ec27210cd6031e403ed
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2023-12-06 10:53:49 +0530

    Add image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,11 @@
 // Home.js 
 import React from 'react'; 
-
+import logo from '../Images/Logo.png'
 
 function Home (){ 
 	return (
         <div>
-          
+          <image src={logo}></image>
         <h1>This is the home123 page</h1>
     </div>
     )
