ec64167ab207551960444a01098bc11cb5e890d8
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2023-12-28 20:45:35 +0530

    add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Topbar from "./components/topbar/Topbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import logo from './Image/newlogo.png'; // Tell webpack this JS file uses this image
 function App() {
   return (
     <Router>
       <Topbar />
-        <h>Hello</h>
+        <p><img src={logo} alt="Logo" /></p>
     </Router>
     
   
