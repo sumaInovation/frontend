3f4eb1af9fb70b6668144f069ce5a4a401eaf92a
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2023-12-28 21:31:42 +0530

    updates1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/Home/Homepage.js";
 import Plc from "./pages/Plc/Plc.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from './Image/newlogo.png'; // Tell webpack this JS file uses this image
 function App() {
   return (
-    <BrowserRouter>
-      <Topbar />
-      <Route path="/" element={<Homepage />} />
-      <Route path="plc" element={<Plc/>}/>
-      </BrowserRouter>
+    <Router>
+        <Topbar />
+      <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    
+
+      
+      </Router>
     
   
    );
