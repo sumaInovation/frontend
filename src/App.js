695e3424b670f540b1f2cb35a6bf208e5d3681a1
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2023-12-29 06:39:38 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Homepage from "./pages/Home/Homepage.js";
 import Cprograming from "./pages/Cprogramming/Cprogramming.js"
 import Plc from "./pages/Plc/Plc.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import logo from './Image/newlogo.png'; // Tell webpack this JS file uses this image
+
 function App() {
   return (
     <Router>
