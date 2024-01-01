8e245e2849ef8b44047425d73de0dde8e01ea908
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-01 17:57:47 +0530

    Commint2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import Cards from "./Cards";
-import Header from "./Header";
+
 import './App.css'
+import Footer from "./Footer"
+import Header from './Header';
 
-
+import Grid1 from "./MyGrid";
 function App() {
     return (
       <div className="App">
+     <Header/>
+     <br/> <br/> <br/> <br/>
+       <Grid1/>
+       <Footer></Footer>
      
-    
        
-        <Cards/>
-         
+   
      
       </div>
     )
