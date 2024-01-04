fe4ab31e67b5cc6bbf3cac4972ced119b21d838a
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-01-04 19:21:49 +0530

    1.5rem

--- src/Component/Banner.js
+++ src/Component/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     <>
     <div className="banner">
     <div className="banner-inner">
-    <h2>WELCOME TO  SUMAAUTOMATIONLK</h2>
+    <h1>WELCOME TO  SUMAAUTOMATIONLK</h1>
     <img src="https://www.hmkdirect.com/images/1_products/control/plc_based_motion/s7-1500_header.pagespeed.1499076938.jpg/rs-960x10000a.jpg"></img>
  
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque maiores repellendus reprehenderit tempora ratione corporis officia expedita eos molestias excepturi?</p>
