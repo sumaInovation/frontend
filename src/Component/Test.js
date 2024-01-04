2a4c1007d403d3f886fca4a71c2d12a74f801ed1
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-01-04 19:37:32 +0530

    adding

--- src/Component/Test.js
+++ src/Component/Test.js
@@ -15,7 +15,7 @@ const Test = () => {
 
 <div className="hero">
   <div className="hero-inner">
-    <h1>Welcome To Our Site!</h1>
+    <h1>Welcome To SUMAAUTOMATIONLK!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nesciunt accusantium tempora voluptatem velit enim quaerat sapiente sed, repellat temporibus eaque dignissimos, saepe ab recusandae incidunt fuga veniam vero vitae!</p>
     <a href="#" class="button">Learn More</a>
   </div>
