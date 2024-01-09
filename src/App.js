eb36742d70d4af66a4caba61934520d72b90b8e1
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-09 11:32:57 +0530

    optimized

--- src/App.js
+++ src/App.js
@@ -7,23 +7,19 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Services from './Pages/Services/Services';
 import Contact from './Pages/Contact/Contact';
 import Footer from './Component/Footer/Footer';
-
-
-
-
 function App() {
   return (
     <>
     
     <BrowserRouter>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Routes>
     <Route path="/" element={<Mainpage/>}></Route>
     <Route path="/About" element={<About/>}></Route>
     <Route path="/Services" element={<Services/>}></Route>
     <Route path="/Contact" element={<Contact/>}></Route>
     </Routes>
-   <Footer/>
+   {/* <Footer/> */}
     </BrowserRouter>
  
 
