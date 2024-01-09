d3f4145261c5348f1e6e15798a56d5b97ee3705a
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-01-09 16:13:03 +0530

    update new version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 
 import './App.css';
-// import Navbar from './Component/Navbar/Navbar';
+import Navbar from './Component/Navbar/Navbar';
 import About from './Pages/About/About';
 import Mainpage from './Pages/MainPage/Mainpage';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Services from './Pages/Services/Services';
 import Contact from './Pages/Contact/Contact';
-// import Footer from './Component/Footer/Footer';
+import Footer from './Component/Footer/Footer';
 function App() {
   return (
     <>
     
     <BrowserRouter>
-    {/* <Navbar/> */}
+    <Navbar/>
     <Routes>
     <Route path="/"  element={<Mainpage/>}></Route>
     <Route path="/About" element={<About/>}></Route>
     <Route path="/Services" element={<Services/>}></Route>
     <Route path="/Contact" element={<Contact/>}></Route>
     </Routes>
-   {/* <Footer/> */}
+   <Footer/>
     </BrowserRouter>
  
 
