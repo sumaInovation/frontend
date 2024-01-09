64dd06baa4b33906e535edc24e538933ba6b235c
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-01-09 20:49:36 +0530

    Adding home

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <div className='home-container'>
+   <div className='PLC'>
+    <h1>PLC Programming</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
+    </p>
+   
+   
+   </div>
+   <div className='C'>
+   <h1>C/C++ Programming</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
+    </p>
+   
+   </div>
+   <div className='Online'>
+   <h1>Online COurses</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
+    </p>
+   </div>
+   <div className='Workshop'>
+   <h1>Industrial Workshop</h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
+    </p>
+   </div>
+   
+   </div>
   )
 }
 
