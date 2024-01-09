8749d43469a91423b17a894a004303b5a1eff8bb
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-01-09 21:46:30 +0530

    part1

--- src/page/Home.js
+++ src/page/Home.js
@@ -3,36 +3,10 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div className='home-container'>
-   <div className='PLC'>
-    <h1>PLC Programming</h1>
-    <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
-    </p>
-   
-   
-   </div>
-   <div className='C'>
-   <h1>C/C++ Programming</h1>
-    <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
-    </p>
-   
-   </div>
-   <div className='Online'>
-   <h1>Online COurses</h1>
-    <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
-    </p>
-   </div>
-   <div className='Workshop'>
-   <h1>Industrial Workshop</h1>
-    <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dicta!
-    </p>
-   </div>
-   
-   </div>
+    <>
+    HERO
+    
+   </>
   )
 }
 
