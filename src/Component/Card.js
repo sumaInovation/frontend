b5c4517c86bddcaddbca168c229160bf20f6f7b5
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-13 00:07:36 +0530

    edited

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -18,7 +18,7 @@ const Card = () => {
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105  duration-75">
           <img src={plc} className="w-[100%] mx-auto mt-[-3rem] bg-white" />
           <h2 className="font-bold text-6xl py-8 text-center">
-            PLC Programming
+            C+C++ Programming
           </h2>
           <p className="text-center">Description and price display here</p>
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
@@ -26,7 +26,7 @@ const Card = () => {
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105  duration-75">
           <img src={plc} className="w-[100%] mx-auto mt-[-3rem] bg-white" />
           <h2 className="font-bold text-6xl py-8 text-center">
-            PLC Programming
+            Online Courses
           </h2>
           <p className="text-center">Description and price display here</p>
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
@@ -34,7 +34,7 @@ const Card = () => {
         <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105  duration-75">
           <img src={plc} className="w-[100%] mx-auto mt-[-3rem] bg-white" />
           <h2 className="font-bold text-6xl py-8 text-center">
-            PLC Programming
+            Industrial Workshop
           </h2>
           <p className="text-center">Description and price display here</p>
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' >More</button>
