f36f09f2b1b73f29026cc3009a07866bf5c29dee
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-12 22:57:11 +0530

    Add_content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
     import React from 'react'
 import Navbar from './Component/Navbar';
 import Hero from './Component/Hero';
+import Content from './Component/Content';
    
     const App=()=> {
       return (
         <>
         <Navbar/>
         <Hero/>
+      <Content/>
         </>
       );
     
---
+++ src/Component/Content.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../Assets/hero-img.png";
+const Content = () => {
+  return (
+    <div className="w-full bg-white py-6 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img src={logo} className='w-[500px] mx-auto my-4' />
+       <div className='flex flex-col justify-center items-center'>
+        <h1 className='text-[#00df9a]'>PLC & Advance Robotic Courses</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse ratione
+          expedita suscipit iusto consequatur in explicabo laborum fuga qui,
+          ullam id itaque voluptas. Doloribus fugit fugiat vel sint, quam
+          repellat?
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
