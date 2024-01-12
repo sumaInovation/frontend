5f80d1650e0261adf28bf6b48bd2a1d3231feebf
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-12 21:15:34 +0530

    Tailwin_navbar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
     import React from 'react'
-    import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './page/Layout';
-import Home from './page/Home';
-import Contact from './page/Contact';
-import NoPage from './page/NoPage';
-import Services from './page/Services';
-import About from './page/About';
-import Footer from './page/Footer';
-import './App.css'
-    
+import Navbar from './Component/Navbar';
+
     const App=()=> {
       return (
-        <BrowserRouter>
-          <Routes>  
-            <Route path="/" element={<Layout/>}>
-              <Route index element={<Home/>} />
-              <Route path="Services" element={<Services/>} />
-              <Route path="contact" element={<Contact/>} />
-              <Route path="About" element={<About/>}/>
-              <Route path="*" element={<NoPage/>} />
-            </Route>
-          </Routes>
-          <Footer/>   
-        </BrowserRouter>
+        <>
+        <Navbar/>
+        </>
       );
     
   
---
+++ src/Component/Navbar.js
@@ -0,0 +1,36 @@
+import React,{useState} from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+const Navbar = () => {
+    const [nav,setNavbar]=useState(false);
+    const handlNavbar=()=>{
+        setNavbar(!nav);
+    }
+  return (
+    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto p-4">
+      <h className="w-full text-3xl font-bold text-[#00df9a] m-4">REACT</h>
+
+      <ul className="flex hidden md:flex">
+        <li className="p-4">HOME</li>
+        <li className="p-4">Courses</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+
+      <div onClick={handlNavbar} className="block md:hidden">
+        {!nav ? <AiOutlineMenu size={20}/>:<AiOutlineClose size={20}/>}
+       
+      </div>
+
+      <div className={nav?"fixed left-0 top-0 w-[60%] border-r border-r-gray-100/50 h-full ease-out duration-500":"fixed left-[-100%]"}>
+        <ul className="p-20 uppercase">
+          <li className="p-4 border-b border-b-gray-600">HOME</li>
+          <li className="p-4 border-b border-b-gray-600">Courses</li>
+          <li className="p-4 border-b border-b-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
