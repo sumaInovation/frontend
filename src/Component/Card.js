d061421a01129a4f2aed6a4ceee52a30282d0360
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-14 19:14:51 +0530

    correction

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -8,39 +8,42 @@ const Card = () => {
     <div className="w-full py-[10rem] bg-white">
       <div className="max-w-[1240px] mx-auto grid  md:grid-cols-2 gap-8">
         <div className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105">
-          {/* <img src={plc} className="w-[100%] mx-auto mt-[-3rem] bg-white" /> */}
-          <h2 className="py-8 text-6xl font-bold text-center">
+          <img src={plc} className="w-[500px] mx-auto mt-[-3rem] bg-white" />
+          <h2 className="py-8 text-2xl font-bold text-center md:text-5xl">
             PLC Programming
           </h2>
-          <p className="text-center ">Duration:4months</p>
-          <p className="text-center">Fees:25000LKR</p>
+          <p className="text-center ">Duration:4months<br/>Fess:25000LKR</p> 
+          
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
         </div>
         <div className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105">
-          {/* <img src={mc} className="w-[100%] mx-auto mt-[-3rem] bg-white" /> */}
-          <h2 className="py-8 text-6xl font-bold text-center">
+          <img src={mc} className="w-[500px] mx-auto mt-[-3rem] bg-white" />
+          <h2 className="py-8 text-2xl font-bold text-center md:text-5xl">
             C/C++ Programming
           </h2>
-          <p className="text-center ">Duration:4months</p>
-          <p className="text-center">Fees:25000LKR</p>
+          <p className="text-center ">Duration:4months<br/>Fess:25000LKR</p> 
+          
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
         </div>
         <div className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105">
-          {/* <img src={workshop} className="w-[100%] mx-auto mt-[-3rem] bg-white" /> */}
-          <h2 className="py-8 text-6xl font-bold text-center">
+          <img src={online} className="w-[500px] mx-auto mt-[-3rem] bg-white" />
+          <h2 className="py-8 text-2xl font-bold text-center md:text-5xl">
+           Online Courses
+          </h2>
+          <p className="text-center ">Duration:4months<br/>Fess:25000LKR</p> 
+          
+          <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
+        </div>
+        <div className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105">
+          <img src={workshop} className="w-[500px] mx-auto mt-[-3rem] bg-white" />
+          <h2 className="py-8 text-2xl font-bold text-center md:text-5xl">
             Industrial Workshop
           </h2>
-          <p className="text-center">Description and price display here</p>
+          <p className="text-center ">Duration:4months<br/>Fess:25000LKR</p> 
+          
           <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' > More</button>
         </div>
-        <div className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105">
-          {/* <img src={online} className="w-[100%] mx-auto mt-[-3rem] bg-white" /> */}
-          <h2 className="py-8 text-6xl font-bold text-center">
-            Online Courses
-          </h2>
-          <p className="text-center">Description and price display here</p>
-          <button  className='bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3' >More</button>
-        </div>
+       
       </div>
     </div>
   );
