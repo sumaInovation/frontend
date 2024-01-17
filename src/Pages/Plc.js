3f271a49a15d5a48696c1c6851ea19487bf5deb2
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-17 19:58:23 +0530

    adiing nav to plc tab

--- src/Pages/Plc.js
+++ src/Pages/Plc.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Plc = () => {
   return (
+    <div>
     <div className="w-full py-[10rem] bg-white">
       <h2 className="py-8 text-5xl font-bold text-center md:text-7xl text-[#009B77]">
         PLC PROGRAM
@@ -26,6 +27,45 @@ const Plc = () => {
         <p>Can be pay on 4 months</p>
         </div>
       </div>
+    </div>
+
+{/* Navbar */}
+<div>
+
+
+
+
+   
+ 
+<ul class="block text-sm font-medium text-center text-gray-500 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+    <li class="w-full ">
+        <a  onClick={()=>{alert('hello')}}  href="#" class="inline-block w-full p-4 text-gray-900 bg-gray-100 border-r border-gray-200 dark:border-gray-700 rounded-s-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" aria-current="page">SYLABUS</a>
+    </li>
+    <li class="w-full">
+        <a onClick={()=>{alert('hello')}}  href="#" class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">GALLERY</a>
+    </li>
+    <li class="w-full">
+        <a onClick={()=>{alert('hello')}}  href="#" class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">More Deatils</a>
+    </li>
+   
+</ul>
+
+
+  
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+    
     </div>
   );
 };
