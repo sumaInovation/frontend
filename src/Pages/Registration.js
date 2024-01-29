51df7472c74b4a69d5b860b3ab82415f0ec39a85
sumanga wimaladasa <sumanga0000@gmail.com>
2024-01-29 21:18:01 +0530

    edited

--- src/Pages/Registration.js
+++ src/Pages/Registration.js
@@ -24,13 +24,6 @@ const Registration = () => {
       .catch((error) => {alert('Error')});
     
   };
-  
-
-  
-
-
-  
-
   return (
     <div className="grid min-h-screen bg-white place-items-center">
       <div className="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
