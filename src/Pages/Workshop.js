54fcc0c009c4b5235636d77d0f070c0186495456
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-02-05 16:09:11 +0530

    edited

--- src/Pages/Workshop.js
+++ src/Pages/Workshop.js
@@ -2,14 +2,12 @@ import {useLocation} from 'react-router-dom';
 import { useEffect } from "react";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import Sylabus from "./Sylabus";
 import { Link, Navigate } from "react-router-dom";
-import Advanceplc from "./Advanceplc";
+
 import Typed from "react-typed";
 import background from "../Assets/worksop1.jpg";
 const Workshop = () => {
   const [isTab, setIsTab] = useState(0);
-   const location = useLocation();
  const { pathname } = useLocation();
  useEffect(() => {
    window.scrollTo(0, 0);
