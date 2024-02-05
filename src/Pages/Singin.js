b41fbcea7f225aeec40a80b9026bd3c1f766111a
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-02-05 17:00:10 +0530

    edited

--- src/Pages/Singin.js
+++ src/Pages/Singin.js
@@ -5,8 +5,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import im4 from '../Assets/im4.png'
 export default function Singup() {
-    const location = useLocation();
 const { pathname } = useLocation()
+const [isComplete,setIsComplete]=useState(1)
 useEffect(() => {
   window.scrollTo(0, 0);
 }, [pathname]);
@@ -37,10 +37,11 @@ useEffect(() => {
         
         
     }
+    if(isComplete===0){
     return (
         <>
           
-            <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
+              <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
                         className="w-auto  mx-auto h-36"
@@ -117,4 +118,23 @@ useEffect(() => {
           
         </>
     )
+                }
+                else{
+                
+                    return (
+                        <>
+                          <div class="flex h-screen w-screen items-center justify-center">
+                            <button type="button" class="flex items-center rounded-lg bg-green-700 px-4 py-2 text-white" disabled>
+                              <svg class="mr-3 h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                              </svg>
+                              <span class="font-medium"> Processing... </span>
+                            </button>
+                          </div>
+                        </>
+                      );
+
+
+                }
 }
