7e5a8c53eb3c48c70ffc724b8fdcde1af78af301
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-02-05 16:32:49 +0530

    edited

--- src/Pages/Singin.js
+++ src/Pages/Singin.js
@@ -3,6 +3,7 @@ import {useLocation} from 'react-router-dom';
 import { useEffect } from "react";
 import React, { useState } from "react";
 import axios from "axios";
+import im4 from '../Assets/im4.png'
 export default function Singup() {
     const location = useLocation();
 const { pathname } = useLocation()
@@ -42,16 +43,17 @@ useEffect(() => {
             <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
-                        className="w-auto h-10 mx-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        className="w-auto  mx-auto h-36"
+                        // src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        src={im4}
                         alt="Your Company"
                     />
-                    <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
+                    <h2 className="mt-5 text-2xl font-bold leading-9 tracking-tight text-center text-[#ebf4eb]">
                         Sign in to your account
                     </h2>
                 </div>
 
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form className="space-y-6" onSubmit={Handlesubmit} >
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
