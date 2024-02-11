aba73a6698fccd0b549da9055681818993142271
sumanga wimaladasa <sumanga0000@gmail.com>
2024-02-11 14:59:07 +0530

    edited

--- src/Pages/Workshop.js
+++ src/Pages/Workshop.js
@@ -1,4 +1,4 @@
-import {useLocation} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
@@ -8,18 +8,17 @@ import Typed from "react-typed";
 import background from "../Assets/worksop1.jpg";
 const Workshop = () => {
   const [isTab, setIsTab] = useState(0);
- const { pathname } = useLocation();
- useEffect(() => {
-   window.scrollTo(0, 0);
- }, [pathname]);
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <div>
-    
       <div
-        className="flex flex-col items-center bg-white bg-center bg-no-repeat bg-cover md:h-screen" 
-        style={{ backgroundImage:`url(${background})`}}
-        >
-          <h2 className="py-8 text-4xl font-bold text-center text-white md:text-7xl">
+        className="flex flex-col items-center bg-white bg-center bg-no-repeat bg-cover md:h-screen"
+        style={{ backgroundImage: `url(${background})` }}
+      >
+        <h2 className="py-8 text-4xl font-bold text-center text-white md:text-7xl">
           <Typed
             strings={["PLC WORKSHOPS"]}
             typeSpeed={120}
@@ -28,40 +27,33 @@ const Workshop = () => {
           />
         </h2>
 
-          <motion.div
-            initial={{ y: "-20vh" }}
-            animate={{ y: 0 }}
-            transition={{ duration: 1 }}
-            whileHover={{scale:1.2}}
-            className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105 md:w-[600px]"
-          >
-            
-            
-          
-            <h2 className="py-8 text-2xl font-bold text-center bg-[#2476D8] md:text-3xl text-white">
-               Industrial Workshop
-            </h2>
-            <p className="font-semibold text-center text-[1.5rem]   inline-block   bg-orange-100 ">
-            Now you can join our one day workshop <br/>and enhance your skills
-             
-             
-            </p>
-            
-          </motion.div>
+        <motion.div
+          initial={{ y: "-20vh" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 1 }}
+          whileHover={{ scale: 1.2 }}
+          className="flex flex-col w-full p-4 my-4 duration-75 rounded-lg shadow-xl hover:scale-105 md:w-[600px]"
+        >
+          <h2 className="py-8 text-2xl font-bold text-center bg-[#2476D8] md:text-3xl text-white">
+            Industrial Workshop
+          </h2>
+          <p className="font-semibold text-center text-[1.5rem]   inline-block   bg-orange-100 ">
+            Now you can join our one day workshop <br />
+            and enhance your skills
+          </p>
+        </motion.div>
 
-        
         <h2 className="py-8 text-2xl font-bold text-center text-[#26d223d5] md:text-5xl">
-         The Pioneer Of Your Engineering Dream 
+          The Pioneer Of Your Engineering Dream
         </h2>
-        <motion.button  
-        whileHover={{backgroundColor:"blue",scale:1.5}}
-        
-        
-        className='bg-gradient-to-r from-green-400
-         to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... w-[200px] rounded-md mx-auto my-6 py-3 text-[#000300]' >
-          <Link to='/contact'>Contact Us</Link></motion.button>
+        <motion.button
+          whileHover={{ backgroundColor: "blue", scale: 1.5 }}
+          className="bg-gradient-to-r from-green-400
+         to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... w-[200px] rounded-md mx-auto my-6 py-3 text-[#000300]"
+        >
+          <Link to="/contact">Contact Us</Link>
+        </motion.button>
       </div>
-      
 
       {/* Navbar */}
       <div>
