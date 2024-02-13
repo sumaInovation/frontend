f48bb438bdee4c55df0bb9a0b312c0fed2373299
sumanga wimaladasa <sumanga0000@gmail.com>
2024-02-13 11:54:44 +0530

    edited

--- src/Pages/Kidsrobotics.js
+++ src/Pages/Kidsrobotics.js
@@ -67,58 +67,9 @@ const Online = () => {
       </div>
       
 
-      {/* Navbar */}
-      <div>
-        <ul class="block text-sm font-medium text-center text-gray-500 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-          <li class="w-full ">
-            <button
-              onClick={() => {
-                setIsTab(1);
-              }}
-              class="inline-block w-full p-4 text-gray-900 bg-gray-100 border-r border-gray-200 dark:border-gray-700 rounded-s-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white"
-              aria-current="page"
-            >
-              PLC BEGINER LEVEL
-            </button>
-          </li>
-          <li class="w-full">
-            <button
-              onClick={() => {
-                setIsTab(2);
-              }}
-              class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              PLC ADVANCE LEVEL
-            </button>
-          </li>
-          <li class="w-full">
-            <button
-              onClick={() => {
-                setIsTab(3);
-              }}
-              class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-            ROBOTICS BEGINER LEVEL
-            </button>
-          </li>
-          <li class="w-full">
-            <button
-              onClick={() => {
-                setIsTab(4);
-              }}
-              class="inline-block w-full p-4 bg-white border-r border-gray-200 dark:border-gray-700 hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-              ROBORTICS ADVANCE LEVEL
-            </button>
-          </li>
-        </ul>
-      </div>
+     
 
-      {/* Content */}
-      {isTab === 1 && <div className="text-white"> PLC BEGINER LEVEL</div>}
-      {isTab === 2 && <div className="text-white"> PLC ADVANCE LEVEL</div>}
-      {isTab === 3 && <div className="text-white">ROBORTICS BEGINER LEVEL</div>}
-      {isTab === 4 && <div className="text-white"> ROBORTICS ADVANCE LEVEL </div>}
+      
     </div>
   );
 };
