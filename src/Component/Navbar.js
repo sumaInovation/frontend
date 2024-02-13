e60f3dadd90e27e2ab6ccd0643183dfb1c961dd1
sumanga wimaladasa <sumanga0000@gmail.com>
2024-02-13 09:01:02 +0530

    edited

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
             <Link to="/about">About</Link>
           </li> */}
           <li className="p-4 rounded-md hover:bg-sky-700">
-            <Link to="/registration">REGISTER</Link>
+            <Link to="/registration">SING UP</Link>
           </li>
          <li className="p-4">
             <Link to="/singin" >
@@ -65,55 +65,59 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-100/50 h-full ease-out duration-500"
+              ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-100/50 h-full ease-out duration-500 bg-[#e6eae6de] z-40 "
               : "fixed hidden"
           }
         >
-          <ul className="p-20 uppercase text-[#c90c0cbf] font-bold">
-            <li className="p-4 border-b border-b-gray-600">
+          
+          <ul className="p-20 uppercase text-[#0c0c0cbf] font-bold">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/" onClick={handlNavbar}>
                 HOME
               </Link>
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/plc" onClick={handlNavbar}>
                 PLC{" "}
               </Link>{" "}
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/cpro" onClick={handlNavbar}>
                 Robotics
               </Link>
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/online" onClick={handlNavbar}>
                 Online{" "}
               </Link>
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/kids" onClick={handlNavbar}>
                 Kid Robotics{" "}
               </Link>
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/workshop" onClick={handlNavbar}>
                 Workshop
               </Link>{" "}
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/about" onClick={handlNavbar}>
                 About
               </Link>
             </li>
-            <li className="p-4 border-b border-b-gray-600">
+            <li className="p-4 border-b border-b-gray-600 hover:bg-sky-700">
               <Link to="/registration" onClick={handlNavbar}>Register</Link>
             </li>
-            <li className="p-4">
+            <li className="p-4 border-b hover:bg-sky-700 border-b-gray-600">
               <Link to="/singin" onClick={handlNavbar}>
                 SING IN
               </Link>
             </li>
-            <li className="p-4 ">
+            <li className="p-4 border-b hover:bg-sky-700 border-b-gray-600">
+            <Link to="/registration" onClick={handlNavbar}>SING UP</Link>
+          </li>
+            <li className="p-4 hover:bg-sky-700">
               <Link to="/contact" onClick={handlNavbar}>
                 Contact
               </Link>
