11487b26b10b121fc517ed8de9489e5a5c72ee47
sumaInovation <136235586+sumaInovation@users.noreply.github.com>
2024-02-21 06:46:05 +0530

    edited

--- src/Pages/Advancerobotics.js
+++ src/Pages/Advancerobotics.js
@@ -47,15 +47,15 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Basic C/C++
           </h2>
-          1.0 Keywords and Identifiers
-          <br /> 1.1 Operators
-          <br /> 1.2 Constants
-          <br />
-          1.3 Variables
-          <br />
-          1.4 Data Types
-          <br /> 1.5 Precedence of Operators
-          <br /> 1.6 Scope and Lifetime of Variables
+          <ul className="text-start">
+          <li>1.0 Keywords and Identifiers</li>
+           <li>1.1 Operators</li>
+           <li>1.2 Constants</li>
+           <li>1.3 Variables</li>
+           <li>1.4 Data Types</li>
+           <li>1.5 Precedence of Operators</li>
+           <li>1.6 Scope and Lifetime of Variables</li>
+           </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -68,17 +68,20 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Control Statement and Expressions
           </h2>
-          2.0 Decision Making using if statement
-          <br />
-          2.1 Types of if …else block
-          <br />
-          2.2 Switch case Block
-          <br />
-          2.3 Arithmetic Expressions
-          <br />
-          2.4 Evaluation of Expressions
-          <br />
-          GOTO statement
+         < ul className="text-start">
+
+          <li>2.0 Decision Making using if statement</li>
+          
+          <li>2.1 Types of if …else block</li>
+          
+          <li>2.2 Switch case Block</li>
+          
+          <li>2.3 Arithmetic Expressions</li>
+          
+          <li>2.4 Evaluation of Expressions</li>
+          
+          <li>GOTO statement</li>
+          </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -91,14 +94,14 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Looping
           </h2>
-          3.0 Concept of Loop
-          <br />
-          3.1 For loop <br />
-          3.2 While loop <br />
-          3.3 Do while loop <br />
-          3.4 Jumping in Loop
-          <br />
-          3.5 break and continue statement
+          <ul className="text-start">
+          <li>3.0 Concept of Loop</li>
+          <li>3.1 For loop</li> 
+          <li>3.2 While loop</li> 
+          <li>3.3 Do while loop</li> 
+          <li>3.4 Jumping in Loop</li>
+          <li>3.5 break and continue statement</li>
+          </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -111,14 +114,13 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Arrays and String
           </h2>
-          4.0 Introduction of Array & String
-          <br />
-          4.1 One - D Array <br />
-          4.2 Two - D Array
-          <br />
-          4.3 Multidimensional Array
-          <br />
-          4.3 String
+          <ul className="text-start">
+          <li>4.0 Introduction of Array & String</li>
+          <li>4.1 One - D Array</li> 
+          <li>4.2 Two - D Array</li>
+          <li>4.3 Multidimensional Array</li>
+          <li>4.3 String</li>
+          </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -131,17 +133,15 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Functions
           </h2>
-          5.0 Concept of Function
-          <br />
-          5.1 Type of Functions
-          <br />
-          5.2 User Defined Function
-          <br />
-          5.3 System Define Functions
-          <br />
-          5.4 Types of parameter
+          <ul className="text-start">
+          <li>5.0 Concept of Function</li>
+          <li>5.1 Type of Functions</li>
+          <li>5.2 User Defined Function</li>
+          <li>5.3 System Define Functions</li>
+          <li>5.4 Types of parameter</li>
+          </ul>
         </motion.div>
-        <motion.div
+<motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ scale: 1, opacity: 1 }}
           transition={{ delay: 1, duration: 0.5 }}
@@ -152,19 +152,20 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Pointers
           </h2>
-          6.0 Need of Pointers
-          <br />
-          6.1 Types of Pointers
-          <br />
-          6.2 Pointer Expression
-          <br />
-          6.3 Arrays of Pointers
-          <br />
-          6.7 Pointers and Functions
+          <ul className="text-start">
+          <li>6.0 Need of Pointers</li>
+          <li>6.1 Types of Pointers</li>
+          <li>6.2 Pointer Expression</li>
+          <li>6.3 Arrays of Pointers</li>
+          <li>6.7 Pointers and Functions</li>
+          </ul>
         </motion.div>
         <motion.div
+          
           initial={{ opacity: 0, scale: 0 }}
+          
           animate={{ scale: 1, opacity: 1 }}
+          
           transition={{ delay: 1, duration: 0.5 }}
           className="flex flex-col w-full p-4 my-4 text-center 
           duration-75 rounded-lg shadow-xl hover:scale-105
@@ -173,15 +174,13 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Structure and Unions
           </h2>
-          7.0 Need of Structure
-          <br />
-          7.1 Implementing Structure Variable
-          <br />
-          7.2 Arrays of Structure
-          <br />
-          7.3 Structure within Structure <br />
-          7.4 Introduction of Unions
-          <br />
+          <ul className="text-start">
+          <li>7.0 Need of Structure</li>
+          <li>7.1 Implementing Structure Variable</li>
+           <li>7.2 Arrays of Structure</li>
+           <li>7.3 Structure within Structure</li>
+          <li>7.4 Introduction of Unions</li>
+        </ul>
           7.5 Diffrent between union & structure
         </motion.div>
         <motion.div
@@ -195,11 +194,11 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             File Handling using 'C'
           </h2>
-          8.0 Opening and Closing File
-          <br />
-          8.1 Input / Output operations on File
-          <br />
-          8.2 Command Line Arguments
+          <ul className="text-start">
+          <li>8.0 Opening and Closing File</li>
+          <li>8.1 Input / Output operations on File</li>
+          <li>8.2 Command Line Arguments</li>
+          </ul>
         </motion.div>
 
         <motion.div
@@ -213,11 +212,11 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Dynamic Memory Allocation
           </h2>
-          9.0 Concept of Dynamic Allocation
-          <br />
-          9.1 Implementing Malloc and Calloc Functions
-          <br />
-          9.2 Releasing the free space
+        <ul className="text-start">
+        <li>9.0 Concept of Dynamic Allocation</li>
+         <li>9.1 Implementing Malloc and Calloc Functions</li>
+         <li>9.2 Releasing the free space</li>
+         </ul>
         </motion.div>
 
         <motion.div
@@ -231,12 +230,13 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Introduction to Object Oriented Programming
           </h2>
-          10.0 Concept of OOP
-          <br />
-          10.1 Features of OOP
-          <br />
-          10.2 Custom Libbaries
-          <br />
+          <ul className="text-start">
+          <li>10.0 Concept of OOP</li>
+         
+          <li>10.1 Features of OOP</li>
+      
+          <li>10.2 Custom Libbaries</li>
+          </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -249,12 +249,11 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Introduction to WEB
           </h2>
-          11.0 HTTP
-          <br />
-          11.2 HTTP Web Server
-          <br />
-          11.3 Websockets & Sockets IO
-          <br />
+          <ul className="text-start">
+          <li>11.0 HTTP</li>
+          <li>11.2 HTTP Web Server</li>
+          <li>11.3 Websockets & Sockets IO</li>
+          </ul>
         </motion.div>
 
         <motion.div
@@ -269,13 +268,12 @@ const Advancerobotics = () => {
             Practicals
           </h2>
           <h3 className="font-bold">Arduino base Motor Controlling</h3>
-          12.1 DC motor controlling
-          <br />
-          12.2 Stepper Motor Controlling
-          <br />
-          12.3 PWM Base Controlling technique
-          <br/>
-          12.4 Servo Motor Controlling   
+          <ul className="text-start">
+          <li>12.1 DC motor controlling</li>
+          <li>12.2 Stepper Motor Controlling</li>
+          <li>12.3 PWM Base Controlling technique</li>
+          <li>12.4 Servo Motor Controlling</li> 
+            </ul>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
@@ -288,13 +286,12 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Practicals
           </h2>
-          <h3 className="font-bold">RS 232 Communication on Arduino</h3>
-          13.0 DC Motor Speed & Direction control via RS232
-          <br />
-          13.1 Adruino to Arduino Communication<br/>
-          <br/>
-          13.2 Wireless Communication
-            
+          <ul className="text-start">
+          <li><h3 className="font-bold">RS 232 Communication on Arduino</h3></li>
+          <li>13.0 DC Motor Speed & Direction control via RS232</li>
+          <li>13.1 Adruino to Arduino Communication</li>
+          <li>13.2 Wireless Communication</li>
+            </ul>
         </motion.div>
       
         <motion.div
@@ -308,15 +305,17 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Practicals
           </h2>
-          <h3 className="font-bold">RFID & GSM Modules</h3>
-          14.0 Home automation system controll via HTTP
-          <br />
-          14.1 Arduino and GSM-based Security Alert message to multiple numbers<br/>
-          <br/>
-          14.2 GSM and GPS based  location tracking
-          <br/>
-          14.3 RFID and GSM-based Students Attendance<br/> System with vb.net application
-            
+          <ul className="text-start">
+
+          <li><h3 className="font-bold">RFID & GSM Modules</h3></li>
+          <li>14.0 Home automation system controll via HTTP</li>
+       
+          <li>14.1 Arduino and GSM-based Security Alert message to multiple numbers</li>
+        
+          <li>14.2 GSM and GPS based  location tracking</li>
+        
+          <li>14.3 RFID and GSM-based Students Attendance<br/> System with vb.net application</li>
+            </ul>
         </motion.div>
       
         <motion.div
@@ -330,11 +329,13 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Practicals
           </h2>
-          <h3 className="font-bold">Finger print sensor</h3>
-          15.0 Door lock system 
-          <br />
-          15.1 Students Attendance<br/> System with vb.net application<br/>
+          <ul className="text-start">
+
+          <li><h3 className="font-bold">Finger print sensor</h3></li>
+          <li>15.0 Door lock system </li>
          
+          <li>15.1 Students Attendance<br/> System with vb.net application</li>
+         </ul>
             
         </motion.div>
        
@@ -349,13 +350,12 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Practicals
           </h2>
-          <h3 className="font-bold">Web controll Project</h3>
-          16.0 DC Motor Speed & Direction control via web
-          <br />
-          16.1 Factory data monitoring through online<br/>
-          <br/>
-          16.2 Real time data monitoring via socket IO
-            
+          <ul className="text-start">
+         <li> <h3 className="font-bold">Web controll Project</h3></li>
+          <li>16.0 DC Motor Speed & Direction control via web</li>
+           <li>16.1 Factory data monitoring through online</li>
+            <li>16.2 Real time data monitoring via socket IO</li>
+            </ul>
         </motion.div>
 
         <motion.div
@@ -369,13 +369,14 @@ const Advancerobotics = () => {
           <h2 className="py-8 text-base font-bold text-center md:text-3xl">
             Practicals
           </h2>
+          <ul className="text-start">
           <h3 className="font-bold">STM32 Base Project</h3>
           16.0 DC Motor Speed & Direction control via WIFI
           <br />
           16.1 Factory data monitoring through WIFI<br/>
           <br/>
           16.2 Real time data monitoring via WIFI
-            
+            </ul>
         </motion.div>
       
         <motion.div
@@ -391,9 +392,9 @@ const Advancerobotics = () => {
           </h2>
           <h3 className="font-bold">Arduino Base Project</h3>
           17.0 Line following robot controll
-          <br />
-          17.1 Obstacle Avoiding Robot<br/>
-          <br/>
+        
+          17.1 Obstacle Avoiding Robot
+         
           17.2 Arduino-Controlled Model Elevator
             
         </motion.div>
