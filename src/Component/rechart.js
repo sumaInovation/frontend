545b53b1927db3e28ad8888a915c58efff4395b1
sumaautomationLK <sumanga0000@gmail.com>
2024-11-28 15:06:23 +0530

    Update rechart.js

--- src/Component/rechart.js
+++ src/Component/rechart.js
@@ -1,37 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const RealTimeChart = (data) => {
+const RealTimeChart = (data1) => {
 
+const data=[
+  {
+    time:0,
+    value:100
+  },
+  {
+    time:1,
+    value:80
+  },
+  {
+    time:2,
+    value:30
+  },
+  {
+    time:3,
+    value:100
+  }
+     
+]
   
-  const [counter, setCounter] = useState(0);
-
-  // Simulate real-time data
-  // const fetchData = () => {
-    // Simulating dynamic data from an API or WebSocket
-    
-  //   const newDataPoint = {
-  //     time: new Date().toLocaleTimeString(),
-  //     value:(incommingdata!=undefined?parseInt(incommingdata,10):0)
-  //   };
-  //   setData((prevData) => {
-  //     // Keep only the last 10 data points
-  //     const newData = [...prevData, newDataPoint];
-  //     return newData.length > 10 ? newData.slice(1) : newData;
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   // Fetch data every second
-  //   const intervalId = setInterval(() => {
-  //     fetchData();
-  //     setCounter((prevCounter) => prevCounter + 1); // To trigger the component to re-render
-  //   }, 1000);
-   
-  //   // Clear interval on component unmount
-  //   return () => clearInterval(intervalId);
-  // }, []);
-
+  
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={data}>
